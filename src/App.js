38c3afce7d6c93735cf9bf20b875979570004899
Ayano KAITO <ayano.kaito@codeberry.co.jp>
2020-08-11 09:13:21 +0900

    stateをクリックで表示の切り替え（P.154-156）

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import Rect from './Rect';
 import './App.css';
 
 class App extends Component {
-  msgStyle = {
+  msgStyle1 = {
     fontSize:"24pt",
     color:"#900",
+    margin:"20px 0px",
+    padding: "5px",
+    borderBottom:"2px solid #900"
+  }
+  msgStyle2 = {
+    fontSize:"20pt",
+    color:"white",
+    backgroundColor:"#900",
     margin:"20px 0px",
     padding: "5px",
     borderBottom:"2px solid #900"
@@ -20,21 +28,27 @@ class App extends Component {
     this.state = {
       counter: 0,
       msg: 'count start!',
+      flg: true,
     };
     this.doAction = this.doAction.bind(this)
   }
 
-  doAction(e) {
+  doAction() {
     this.setState((state) => ({
       counter: state.counter + 1,
-      msg: 'count: ' + state.counter
+      msg: state.counter,
+      flg: !state.flg
     }));
   }
 
   render(){
     return <div>
       <h1>React</h1>
-      <p style={this.msgStyle}>{this.state.msg}</p>
+      {this.state.flg ?
+        <p style={this.msgStyle1}>count: {this.state.msg}</p>
+        :
+        <p style={this.msgStyle2}>{this.state.msg}です。</p>
+      }
       <button style={this.btnStyle} onClick={this.doAction}>Click</button>
     </div>;
   }
