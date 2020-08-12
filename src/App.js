5e95aa65d8977870bc630b3eff6e7b871d14d72d
Ayano KAITO <ayano.kaito@codeberry.co.jp>
2020-08-12 09:28:14 +0900

    フォームの利用（P.169-171）

--- src/App.js
+++ src/App.js
@@ -11,14 +11,18 @@ class App extends Component {
     margin:"20px 0px",
     padding: "5px",
   }
+  inputStyle = {
+    fontSize:"12pt",
+    padding:"5px"
+  }
 
-  constructor(props) {
+  constructor(props){
     super(props);
     this.state = {
-      message: 'type your name:'
+      message:'type your name:'
     };
     this.doChange = this.doChange.bind(this);
-    this.doSumit = this.doSubmit.bind(this);
+    this.doSubmit = this.doSubmit.bind(this);
   }
   doChange(event) {
     this.input = event.target.value;
@@ -33,38 +37,14 @@ class App extends Component {
   render() {
     return <div>
       <h1>React</h1>
-      <Message title="Children">
-        これはコンポーネント内のコンテンツです。
-        マルでテキストを分割し、リストにして表示します。
-        改行は必要ありません。
-      </Message>
-    </div>
-  }
-}
-
-class Message extends Component {
-  li = {
-    fontSize:"16pt",
-    color:"#06",
-    margin:"0px",
-    padding: "0px",
-  }
-
-  render() {
-    let content = this.props.children;
-    let arr = content.split('。');
-    let arr2 = [];
-    for(let i = 0; i < arr.length; i++){
-      if(arr[i].trim() != '') {
-        arr2.push(arr[i])
-      }
-    }
-    let list = arr2.map((value, key) => (
-      <li style={this.li} key={key}>{value}</li>)
-    );
-    return <div>
-      <h2>{this.props.title}</h2>
-      <ol>{list}</ol>
+        <h2>{this.state.message}</h2>
+        <form onSubmit={this.doSubmit}>
+          <label>
+            <span style={this.inputStyle}></span>Message:
+            <input type="text" style={this.inputStyle} onChange={this.doChange} />
+          </label>
+          <input type="submit" style={this.inputStyle} value="Click" />
+          </form>
     </div>
   }
 }
