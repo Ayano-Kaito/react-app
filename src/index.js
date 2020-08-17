1d243881e9772cbd8c4083c7037136809e835b3c
Ayano KAITO <ayano.kaito@codeberry.co.jp>
2020-08-17 21:38:16 +0900

    カウンターを永続化のエラー修正

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import Storage from 'redux-persist/lib/storage';
-import { persistGate } from 'redux-persist/integration/react';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 import './index.css';
 import App from './App';
 
