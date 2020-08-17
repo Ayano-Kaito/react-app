5314bd453f81a58868e11167b709885ba24ee7de
Ayano KAITO <ayano.kaito@codeberry.co.jp>
2020-08-17 22:28:39 +0900

    メモアプリを永続化（P.246~247）

--- src/index.js
+++ src/index.js
@@ -2,54 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
-
-let state_value = {
-  counter: 0,
-  message: "COUNTER"
-}
-
-function counter(state = state_value, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-    return {
-      counter: state.counter + 1,
-      message: "INCREMENT"
-    };
-    case 'DECREMENT':
-    return {
-        counter: state.counter - 1,
-        message: "DECREMENT"
-    };
-    case 'RESET':
-    return {
-        counter: 0,
-        message: "RESET"
-    };
-    default:
-    return state;
-  }
-}
+import MemoStore, { memoReducer } from './memo/Store';
 
 const persistConfig = {
-  key: 'root',
-  storage,
-}
+    key: 'memo',
+    storage,
+};
 
-const persistedReducer = persistReducer(persistConfig, counter)
+const persistedReducer = persistReducer(persistConfig, memoReducer);
 
-let store = createStore(persistedReducer)
-let pstore = persistStore(store)
+let store = createStore(persistedReducer);
+let pstore = persistStore(store);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={<p>loading...</p>} persistor={pstore}>
-    <App />
-    </PersistGate>
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <PersistGate loading={<p>loading...</p>} persistor={pstore}>
+            <App />
+        </PersistGate>
+    </Provider>,
+    document.getElementById('root')
 );
+
+export default pstore;
