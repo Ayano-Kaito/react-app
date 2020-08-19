78fc2522cbeaecacf905f929122ae746f71c5e28
Ayano KAITO <ayano.kaito@codeberry.co.jp>
2020-08-20 08:34:43 +0900

    ビルトインCSS（P.274~275）

--- src/next_app/pages/index.js
+++ src/next_app/pages/index.js
@@ -1,19 +1,21 @@
-const h1 = {
-    fontSize: '72pt',
-    fontWeight: 'bold',
-    textAlign: 'right',
-    letterSpacing: '-8px',
-    color: '#f0f0f0',
-    margin: '-40px 0px'
-}
-
-const p = {
-    margin: '0px',
-    color: '#666',
-    fontSize: '16pt'
-}
-
 export default () => <dev>
-    <h1 style={h1}>Next.js</h1>
-    <p style={p}>Welcome to next.js!</p>
+    <style jsx>{`
+    h1 = {
+        fontSize: '72pt',
+        fontWeight: 'bold',
+        textAlign: 'right',
+        letterSpacing: '-8px',
+        color: '#f0f0f0',
+        margin: '-40px 0px'
+    }
+    p = {
+        margin: '0px',
+        color: '#666',
+        fontSize: '16pt'
+    }
+    `}</style>
+
+    <h1>Next.js</h1>
+    <p>Welcome to next.js!</p>
+    <p>これはビルトインCSSによるスタイルです。</p>
 </dev>
